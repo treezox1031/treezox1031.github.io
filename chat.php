56a235dcd27084d7988f081a6a20c554ca50e218
NGINX-del <71128191+NGINX-del@users.noreply.github.com>
2020-09-13 22:40:20 -0700

    Create chat.php

---
+++ chat.php
@@ -0,0 +1,3 @@
+<div class="title">IRC</div>
+<br>
+<iframe class="irc" src="https://kiwiirc.com/client/treezox.strangled.net/?nick=guest|?&theme=cli#your_channel" style="border:0; width:100%; height:450px;"></iframe>
