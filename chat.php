f58b1a614d27f7ac8047b75422847bd0be0f45c1
NGINX-del <71128191+NGINX-del@users.noreply.github.com>
2020-09-13 22:45:05 -0700

    Delete chat.php

--- chat.php
+++
@@ -1,3 +0,0 @@
-<div class="title">IRC</div>
-<br>
-<iframe class="irc" src="https://kiwiirc.com/client/treezox.strangled.net/?nick=guest|?&theme=cli#your_channel" style="border:0; width:100%; height:450px;"></iframe>
